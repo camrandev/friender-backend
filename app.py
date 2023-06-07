0edb7008688ed41733cbc69b3a467e702b468382
Crynowecer0 <crynowecer11@gmail.com>
2023-06-07 14:46:02 -0400

    add uploading files to S3

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from flask import Flask, request, session, g
 from flask_debugtoolbar import DebugToolbarExtension
 from sqlalchemy.exc import IntegrityError
 import boto3
+import tempfile
 
 load_dotenv()
 
@@ -16,7 +17,7 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_ECHO'] = False
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = True
 app.config['SECRET_KEY'] = os.environ['SECRET_KEY']
-toolbar = DebugToolbarExtension(app)
+# toolbar = DebugToolbarExtension(app)
 
 
 # will be built in db model file
@@ -34,17 +35,37 @@ bucket_name = os.environ['S3_BUCKET']
 
 # receive POST file upload from front-end
 # have user in g.user global context
-@app.route('/s3', methods=["GET", "POST"])
+@app.route('/s3', methods= ["POST"])
 def pictures():
     """
     basic route to test our S3 config
     """
 
-# f'users/{g.user.id}/{os.path.basename(file_path)}'
+    # Need to access user ID from request as well, plug that in
+
+    file = request.files['test_file']
+    file_name = file.filename
+    print(f'filename={file_name} type={type(file_name)}')
+    file_content = file.read()
+
+    temp_file = tempfile.NamedTemporaryFile(delete=False)
+    temp_file.write(file_content)
+    temp_file.close()
+
+    print(f"{request.files['test_file'].filename}")
+
     try:
-        s3.upload_file('./test_s3.txt', bucket_name, "test")
+        s3.upload_file(temp_file.name, bucket_name, f'users/1/{file_name}')
         print("File uploaded successfully.")
     except Exception as e:
         print(f"Error uploading file: {str(e)}")
 
-    return s3
+    return 'hi'
+
+@app.route('/s3', methods=["GET"])
+def get_file():
+    """
+    testing getting a file from S3
+    """
+
+    
